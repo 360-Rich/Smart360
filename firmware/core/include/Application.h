8a94aea9f8189f2b128a305db3f061a59dd4bd10
360-Rich <rich@thekelletts.uk>
2026-08-11 20:14:15 +0100

    feat: add motor controller - Application.h

--- firmware/core/include/Application.h
+++ firmware/core/include/Application.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "motor/MotorController.h"
+
 namespace Smart360
 {
-    class Application
-    {
-    public:
-        void begin();
-        void update();
-    };
+class Application
+{
+public:
+    void begin();
+    void update();
+
+private:
+    MotorController motorController;
+};
+
 }
